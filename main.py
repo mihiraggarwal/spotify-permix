1ccfce3d7c03be5a7d1f65108c30529947eff94d
Mihir Aggarwal <mihir.aggarwal1000@gmail.com>
2021-01-21 22:24:06 +0530

    create playlist + add songs

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 import json
 import requests
+import datetime
 import spotipy.util as util
 
 # Figure out a way to get username/token using /authorize
@@ -11,13 +12,14 @@ import spotipy.util as util
 
 username = sys.argv[1]
 number = sys.argv[2]
-scope = 'playlist-read-private'
+scope = 'playlist-read-private playlist-modify-private'
+client_id = "a27eac5f72414c3190b565abd15eb2f1"
 
 try:
-    token = util.prompt_for_user_token(username, scope)
+    token = util.prompt_for_user_token(username, scope, client_id)
 except:
     os.remove(f".cache-{username}")
-    token = util.prompt_for_user_token(username, scope)
+    token = util.prompt_for_user_token(username, scope, client_id)
 
 headers = {"Authorization": f"Bearer {token}"}
 
@@ -26,9 +28,11 @@ res_playlists = requests.get(f'https://api.spotify.com/v1/users/{username}/playl
 respl_json = res_playlists.json()
 
 mix_id = ''
+mix_desc = ''
 for i in respl_json["items"]:
     if i["name"] == f"Daily Mix {number}":
         mix_id = i["id"]
+        mix_desc = i["description"]
         break
 else:
     print(f"Could not find Daily Mix {number} in followed playlists")
@@ -37,9 +41,20 @@ else:
 res_tracks = requests.get(f"https://api.spotify.com/v1/playlists/{mix_id}/tracks", headers = headers)
 restr_json = res_tracks.json()
 
-tracks = {}
-for i in restr_json["items"]:
-    tracks[i["track"]["name"]] = i["track"]["id"]
+uris = [i["track"]["uri"] for i in restr_json["items"]]
 
-print(tracks)
-# print(json.dumps(restr_json, sort_keys = True, indent = 4))
+json = {
+    "name": f"Daily Mix {number} | {datetime.date.today()}",
+    "public": False,
+    "description": mix_desc
+}
+
+res_newpl = requests.post(f"https://api.spotify.com/v1/users/{username}/playlists", headers = headers, json = json)
+resnp_json = res_newpl.json()
+newpl_id = resnp_json["id"]
+
+new_songs = requests.post(f"https://api.spotify.com/v1/playlists/{newpl_id}/tracks", headers = headers, json = {"uris": uris})
+if new_songs.ok:
+    print("Successful")
+else:
+    print("Something went wrong")
